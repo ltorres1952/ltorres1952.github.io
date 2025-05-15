d19181c805fdeb83d710ddf85dd68fbaa328c1b6
Luis Torres <ltorre701@west-mec.org>
2025-05-15 15:53:48 -0700

    First personal

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,8 +1,18 @@
 import { RevealOnScroll } from "../RevealOnScroll";
 
 export const About = () => {
-  const frontendSkills = ["React", "Typescript", "TailwindCSS"];
-  const backendSkills = ["Node.js", "Javascript", "Java", "MongoDB"];
+  const frontendSkills = ["React", "Typescript", "TailwindCSS", "Unity"];
+  const backendSkills = [
+    "Javascript",
+    "Java",
+    "C#",
+    "PHP",
+    "Node.js",
+    "MongoDB",
+    "MySQL",
+    "Laravel",
+    "Express.js",
+  ];
 
   return (
     <section
@@ -10,88 +20,90 @@ export const About = () => {
       className="min-h-screen flex items-center justify-center py-20"
     >
       <RevealOnScroll>
-      <div className="max-w-3xl mx-auto px-4">
-        <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
-          About Me
-        </h2>
+        <div className="max-w-3xl mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+            About Me
+          </h2>
 
-        <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
-          <p className="text-gray-300 mb-6">
-            Passionate developer with expertise in building scalable web
-            applications and creating innovative solutions.
-          </p>
+          <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
+            <p className="text-gray-300 mb-6">
+              Diligent, creative, and adaptable second year coding student who
+              is a great problem solver and able to work well under pressure.
+              Experienced with working in a team, and learning how to use new
+              technology quickly. Seeking the position of Junior Backend
+              Developer.
+            </p>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-              <h3 className="text-xl font-bold mb-4">Frontend</h3>
-              <div className="flex flex-wrap gap-2">
-                {frontendSkills.map((tech, key) => (
-                  <span
-                    key={key}
-                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition"
-                  >
-                    {tech}
-                  </span>
-                ))}
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4">Frontend</h3>
+                <div className="flex flex-wrap gap-2">
+                  {frontendSkills.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition"
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4">Backend</h3>
+                <div className="flex flex-wrap gap-2">
+                  {backendSkills.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition"
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
               </div>
             </div>
+          </div>
 
-            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-              <h3 className="text-xl font-bold mb-4">Backend</h3>
-              <div className="flex flex-wrap gap-2">
-                {backendSkills.map((tech, key) => (
-                  <span
-                    key={key}
-                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition"
-                  >
-                    {tech}
-                  </span>
-                ))}
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
+            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4"> 📚 Education </h3>
+              <ul className="list-disc list-inside text-gray-300 space-y-2">
+                <li>
+                  <strong> Highschool Diploma </strong> - University High
+                  (2021-2025)
+                </li>
+                <li>
+                  Relevant Coursework: AP Computer Science Principles, AP Computer Science A, AP Calculus
+                </li>
+              </ul>
+            </div>
+            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4"> 💼 Work Experience </h3>
+              <div className="space-y-4 text-gray-300">
+                <div>
+                  <h4 className="font-semibold">
+                    Software Engineer at ABC Corp (2020 - Present)
+                  </h4>
+                  <p>
+                    Developed and maintained microservices for cloud-based
+                    applications.
+                  </p>
+                </div>
+
+                <div>
+                  <h4 className="font-semibold">
+                    Intern at DEF Startups (2019)
+                  </h4>
+                  <p>
+                    Assisted in building front-end components and integrating
+                    REST APIs
+                  </p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
-          <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
-            <h3 className="text-xl font-bold mb-4"> 📚 Education </h3>
-            <ul className="list-disc list-inside text-gray-300 space-y-2">
-              <li>
-                <strong> B.S. in Computer Science </strong> - University High
-                (2021-2025)
-              </li>
-              <li>
-                Relevant Coursework: Data Structures, Web Development, Cloud
-                Computing...
-              </li>
-            </ul>
-          </div>
-          <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
-            <h3 className="text-xl font-bold mb-4"> 💼 Work Experience </h3>
-            <div className="space-y-4 text-gray-300">
-              <div>
-                <h4 className="font-semibold">
-                  Software Engineer at ABC Corp (2020 - Present)
-                </h4>
-                <p>
-                  Developed and maintained microservices for cloud-based
-                  applications.
-                </p>
-              </div>
-
-              <div>
-                <h4 className="font-semibold">
-                  Intern at DEF Startups (2019)
-                </h4>
-                <p>
-                  Assisted in building front-end components and integrating REST
-                  APIs
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
       </RevealOnScroll>
     </section>
   );
